c4f4db283b33e378173f6c68da5ae4ca8650567d
Ravindra <ravindra4736@gmail.com>
2026-07-02 23:36:01 +0530

    forntdev

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["dbs_front.test"],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
